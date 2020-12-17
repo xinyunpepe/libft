cb3bb517c0c8c7c44f048a0b508f4f8c4a465028
tvxqunicorn <68616376+tvxqunicorn@users.noreply.github.com>
2020-12-17 10:19:00 +0100

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -20,7 +20,7 @@ char	*ft_strnstr(const	char *haystack, const	char *needle, size_t len)
 	if (!*needle)
 		return ((char *)haystack);
 	i = 0;
-	while (haystack[i] && i < len)
+	while (i < len && haystack[i])
 	{
 		j = 0;
 		if (haystack[i] == needle[j])
